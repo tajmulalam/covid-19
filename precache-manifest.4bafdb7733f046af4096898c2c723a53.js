e8762b7bf2f724790092b0b3deaca5fd700fda3d
tajmulalam <taj.cse37@gmail.com>
2020-04-16 21:16:44 +0200

    Updates

---
+++ precache-manifest.4bafdb7733f046af4096898c2c723a53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cdb337e28395f1d599de88642bcf91c",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "309c0b49636e3b096d42",
+    "url": "/covid-19/static/css/main.5f32f9b4.chunk.css"
+  },
+  {
+    "revision": "4bc37d9bd617d7d4635f",
+    "url": "/covid-19/static/js/2.4e4d4e1e.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19/static/js/2.4e4d4e1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "309c0b49636e3b096d42",
+    "url": "/covid-19/static/js/main.d3371bec.chunk.js"
+  },
+  {
+    "revision": "5947671d1c53a9002efe",
+    "url": "/covid-19/static/js/runtime-main.db8c32d2.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19/static/media/corona.d7265326.png"
+  }
+]);
